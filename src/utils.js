821e3ec91cbe4f0fde7e63cf03ca1c04f2aee99f
Bryce Jech <bryce@brycejech.com>
2018-12-08 08:58:55 -0600

    add a simple merge fn

--- src/utils.js
+++ src/utils.js
@@ -17,4 +17,15 @@ function _type(thing){
     return Object.prototype.toString.call(thing).match(/\[object (.*)\]/)[1].toLowerCase();
 }
 
-export { _pathify, _isPrimitive, _type }
+function _merge(a,b){
+    if(_type(a) !== 'object' || _type(b) !== 'object'){
+        throw new Error('Arguments must be objects');
+    }
+
+    for(const key in a){
+        b[key] = a[key];
+    }
+    return b;
+}
+
+export { _pathify, _isPrimitive, _type, _merge }
